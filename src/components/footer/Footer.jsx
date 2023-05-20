868671eb26b33ff9f5dcbefdaf8f4b54bde6555b
Jibril Omar <jibrilosman404@gmail.com>
2023-05-20 16:59:49 -0400

    Add files via upload

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import './Footer.css'
-import { BsFacebook } from 'react-icons/bs'
-import { AiFillInstagram, AiFillTwitterCircle } from 'react-icons/ai'
+import { BsLinkedin } from 'react-icons/bs'
+import {  AiFillTwitterCircle } from 'react-icons/ai'
+import { FaGithub } from 'react-icons/fa'
+import { SiFrontendmentor } from 'react-icons/si'
 
 const Footer = () => {
     const currentYear = new Date().getFullYear()
   return (
     <footer>
        <div className="footer__socials">
-         <a href="https://facebook.com/jibrilo1" target='_blank' rel="noreferrer"><BsFacebook /></a>
-         <a href="https://www.instagram.com/jibrilo1/" target='_blank' rel="noreferrer"><AiFillInstagram /></a>
+         <a href="https://www.linkedin.com/in/jibril-omar-25bb98216/" target='_blank' rel="noreferrer"><BsLinkedin /></a>
+         <a href="https://github.com/jibrilosman" target='_blank' rel="noreferrer"><FaGithub /></a>
+         <a href="https://www.frontendmentor.io/profile/jibrilosman" target='_blank' rel="noreferrer"><SiFrontendmentor /></a>
          <a href="https://twitter.com/jibrilosman10" target='_blank' rel="noreferrer"><AiFillTwitterCircle /></a>
        </div>
        <div className="copy-right">
