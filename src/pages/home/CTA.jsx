efd2911e0ddde7365d7cc34e3843246c11fa92b4
Jibril Omar <jibrilosman404@gmail.com>
2023-10-07 22:35:57 -0400

    Update CTA.jsx

--- src/pages/home/CTA.jsx
+++ src/pages/home/CTA.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import CV from '../../assets/JIBRIL_OMAR.pdf'
+import CV from '../../assets/JIBRIL OMAR++.pdf'
 import { NavLink } from 'react-router-dom'
 
 const CTA = () => {
