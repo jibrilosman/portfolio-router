50afbc5e15e6cb4eb0f34e0ce8eb67e78f7dfcf5
Jibril Omar <jibrilosman404@gmail.com>
2024-03-26 15:39:21 -0400

    Update CTA.jsx

--- src/pages/home/CTA.jsx
+++ src/pages/home/CTA.jsx
@@ -5,7 +5,7 @@ import { NavLink } from 'react-router-dom'
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={CV}   className="btn">View & Download Resume</a>
+        <a href={CV}   className="btn" target={'_blank'}>View & Download Resume</a>
         <NavLink to="/contact"  className="btn">Contact Me</NavLink>
     </div>
   )
