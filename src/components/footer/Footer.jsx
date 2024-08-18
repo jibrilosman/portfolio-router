7bcf70febc93a1d5a38b4615fdb8fa849f8f832e
Jibril Omar <jibrilosman404@gmail.com>
2024-08-17 20:59:50 -0400

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import './Footer.css'
 import { BsLinkedin } from 'react-icons/bs'
-import { BsTwitterX } from 'react-icons/bs'
-import { FaSquareXTwitter } from "react-icons/fa6";
+import { AiFillTwitterCircle } from 'react-icons/ai'
 import { FaGithub } from 'react-icons/fa'
 import { SiFrontendmentor } from 'react-icons/si'
 
@@ -14,7 +13,7 @@ const Footer = () => {
          <a href="https://www.linkedin.com/in/jibril-omar-25bb98216/" target='_blank' rel="noreferrer"><BsLinkedin /></a>
          <a href="https://github.com/jibrilosman" target='_blank' rel="noreferrer"><FaGithub /></a>
          <a href="https://www.frontendmentor.io/profile/jibrilosman" target='_blank' rel="noreferrer"><SiFrontendmentor /></a>
-         <a href="https://twitter.com/jibrilosman10" target='_blank' rel="noreferrer"><FaSquareXTwitter /></a>
+         <a href="https://twitter.com/jibrilosman10" target='_blank' rel="noreferrer"><AiFillTwitterCircle /></a>
        </div>
        <div className="copy-right">
             <p>© {currentYear} Jibril Omar.</p>
