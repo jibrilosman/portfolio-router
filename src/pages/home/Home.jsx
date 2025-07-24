8f4a7f0d9fe2ca697cf33830d1887cbf39ae8529
Jibril Omar <jibrilosman404@gmail.com>
2025-07-24 11:38:52 -0400

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
         <div className="hero-info">
             <h3>Hi There, I'm</h3>
             <h1>Jibril Omar</h1>
-            <h2>Full-Stack Developer</h2>
+            <h2>Full-Stack Developer & cybersecurity enthusiastic</h2>
             <p>I am a full-stack developer based in Toronto, Canada.
               I have a passion for web development and love to create for web and mobile devices.</p>
               <CTA />
